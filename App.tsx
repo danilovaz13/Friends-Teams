57747c9df320b50e985eefc3d2e8dc08127e9118
Danilo Vaz <danilovaz1305@gmail.com>
2023-11-22 14:14:00 -0300

    Organizando Projeto

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Helloo!</Text>
+    </View>
+  );
+};
+
+export default App;
