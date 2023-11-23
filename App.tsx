a167a0092dd73d3f667f6489efb6e7ad9d4d9334
Danilo Vaz <danilovaz1305@gmail.com>
2023-11-23 13:13:52 -0300

    adicionado themeProvider para toda aplicação

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Groups} from '@screens/Groups';
+import {ThemeProvider} from 'styled-components';
+import theme from './src/theme';
 
 const App = () => {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
+  );
 };
 
 export default App;
