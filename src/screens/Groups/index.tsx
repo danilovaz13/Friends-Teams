368209992b6a189bcd81e1e68d82972b0fc5aa41
Danilo Vaz <danilovaz1305@gmail.com>
2023-11-25 15:00:31 -0300

    Listagem de grupos com FlatList

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,23 @@
+import {useState} from 'react';
+import {FlatList} from 'react-native';
 import {Header} from '@components/Header';
 import {Highlight} from '@components/Highlight';
 import {GroupCard} from '@components/GroupCard';
 import {Container} from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Turma 1', 'Amigos']);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Turma 1" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => String(item)}
+        renderItem={({item}) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
