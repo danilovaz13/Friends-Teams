ed5cb8492493d66330f7928204a9586dc1856445
Danilo Vaz <danilovaz1305@gmail.com>
2023-11-25 17:11:47 -0300

    add componente button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import {Header} from '@components/Header';
 import {Highlight} from '@components/Highlight';
 import {GroupCard} from '@components/GroupCard';
 import {ListEmpty} from '@components/ListEmpty';
+import {Button} from '@components/Button';
 import {Container} from './styles';
 
 export function Groups() {
@@ -23,6 +24,7 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
+      <Button title="Cadastrar nova turma" />
     </Container>
   );
 }
